e7b6533d185d78ffde7252988398114170445859
sei_nanya <seiichiro.nanya@gmail.com>
2020-06-09 13:26:31 +0900

    01

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBsAfWc-HheWScwN6AVF2DcpmMhiL42Qcc",
+    authDomain: "react-firebase-64807.firebaseapp.com",
+    databaseURL: "https://react-firebase-64807.firebaseio.com",
+    projectId: "react-firebase-64807",
+    storageBucket: "react-firebase-64807.appspot.com",
+    messagingSenderId: "762686753153",
+    appId: "1:762686753153:web:d55b4948c78d86349e4080",
+    measurementId: "G-2R18HGXMQ3"
+};
